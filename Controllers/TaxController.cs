40b511d11a6764fb7ba9fdb15728e1e543aa5973
Kieran-Mac <kieran@mac.com>
2017-10-09 08:48:59 +0100

    AddTaxAlgo

--- Controllers/TaxController.cs
+++ Controllers/TaxController.cs
@@ -1,22 +1,29 @@
 ﻿using System;
-using Trader.Models.TaxModels;
-using Trader.Models.TradeImportModels;
 using System.Collections.Generic;
 using System.Linq;
-using Trader.Data;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Trader.Data;
+using Trader.Models.TaxModels;
+using Trader.Models.TradeImportModels;
 
-namespace Trader.Services
+namespace Trader.Controllers
 {
     
-    public class TaxService
+    public class TaxController : Controller
     {
         private readonly ApplicationDbContext _context;
-        public TaxService(ApplicationDbContext context)
+        public TaxController(ApplicationDbContext context)
         {
             _context = context;
         }
-
+        public async Task<IActionResult> Index()
+        {
+            var trades = await _context.TradeImport.ToListAsync();
+            return View(await CalculateTaxEvents(trades));
+        }
         public async Task<IEnumerable<TaxEventModel>> CalculateTaxEvents(IEnumerable<TradeImport> trades)
         {
             var sorted = trades.OrderBy(c => c.TransactionDate);
@@ -32,24 +39,25 @@ namespace Trader.Services
 
                 if (item.type == TransactionType.Buy)
                 {
-                    holder.Append(item);
+                    holder.Add(item);
                 }
                 else
                 {
                     decimal remaining = item.Quantity;
 
-                    foreach (var item2 in holder)
+                    foreach (var item2 in holder.Where(x => x.InstrumentId == item.InstrumentId))
                     {
                         if (remaining > 0)
                         {
                             if(item2.Quantity > remaining)
                             {
-								taxEvents.Append(new TaxEventModel()
+								taxEvents.Add(new TaxEventModel()
 								{
 									TaxEventID = id,
 									Quantity = remaining,
 									StartValue = item2.Value,
                                     EndValue = item.Value,
+                                    Instrument = item.Instrument,
 									StartDate = item2.TransactionDate,
 									EndDate = item.TransactionDate,
 									TaxableValue = 0
@@ -59,12 +67,13 @@ namespace Trader.Services
                             }
                             else
                             {
-								taxEvents.Append(new TaxEventModel()
+								taxEvents.Add(new TaxEventModel()
 								{
 									TaxEventID = id,
 									Quantity = remaining - item2.Quantity,
 									StartValue = item2.Value,
-                                    EndValue = item.Value
+                                    EndValue = item.Value,
+                                    Instrument = item.Instrument,
 									StartDate = item2.TransactionDate,
 									EndDate = item.TransactionDate,
 									TaxableValue = 0
--- Models/TaxModels/TaxEventModel.cs
+++ Models/TaxModels/TaxEventModel.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Trader.Models.TradeImportModels;
+
 namespace Trader.Models.TaxModels
 {
     public class TaxEventModel
@@ -8,9 +10,10 @@ namespace Trader.Models.TaxModels
         public decimal StartValue { get; set; }
         public decimal EndValue { get; set; }
         public decimal Quantity { get; set; }
+        public Instrument Instrument { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
-        public decimal TotalValue { get { return Value * Quantity; } }
+        public decimal TotalValue { get { return EndValue * Quantity; } }
         public decimal TaxableValue { get; set; }
 
         public TaxEventModel()
